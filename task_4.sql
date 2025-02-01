3e1ad79d97dcece354f64b048dec1f055359b98f
njambiEvelyne <evelynenganga76@gmail.com>
2025-02-01 16:51:02 +0300

    Added task_4.sql to print full description of books table using INFORMATION_SCHEMA

--- task_4.sql
+++ task_4.sql
@@ -1,5 +1,8 @@
 -- Use the database passed as argument
+-- Use the database passed as argument
 USE alx_book_store;
 
--- Get full description of the books table
-SHOW COLUMNS FROM books;
+-- Get full description of the books table using INFORMATION_SCHEMA
+SELECT COLUMN_NAME, COLUMN_TYPE
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'Books';
